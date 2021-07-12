5f63bee43cccd81d85a853f519ee5365cf5ed799
Rohit Patro <rohitpatro6@gmail.com>
2021-07-12 11:59:44 +0530

    Employee Daily Wage

---
+++ src/EmployeeWageBuilder/EmployeeWageBuilder.java
@@ -0,0 +1,11 @@
+package EmployeeWageBuilder;
+
+public class EmployeeWageBuilder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Daily Wage");
+		// TODO Auto-generated method stub
+
+	}
+
+}
