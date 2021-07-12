c1baa1721949a9767b6b3cfa59d03b303e79b29f
Rohit Patro <rohitpatro6@gmail.com>
2021-07-12 17:10:40 +0530

    Added:UC1 Employee Attendance Added

--- src/EmployeeWageBuilder/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder/EmployeeWageBuilder.java
@@ -4,7 +4,12 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Daily Wage");
-		
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == IS_FULL_TIME)
+			System.out.println("Employee Is Present");
+		else
+			System.out.println("Employee Is Not Present");
 	}
 
 }
