e6113f6cce7d1a75b64321bf6135939fa6bb7ed5
Rohit Patro <rohitpatro6@gmail.com>
2021-07-13 17:44:54 +0530

    Added:UC5 Wages For 20Days Added

--- src/EmployeeWageBuilder/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder/EmployeeWageBuilder.java
@@ -4,13 +4,16 @@ public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	private static final int NUM_OF_WORKING_DAYS = 2;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Daily Wage");
 		
 		int empHrs = 0;
 		int empWage = 0;
-		int empCheck =(int) Math.floor(Math.random()*10)%3;
-		
+		int totalEmpWage = 0;
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		int empCheck =(int) Math.floor(Math.random()*10) % 3;
 		switch (empCheck) {
 		case IS_FULL_TIME:
 			empHrs = 8;
@@ -22,7 +25,10 @@ public class EmployeeWageBuilder {
 			empHrs = 0;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage:" + empWage);
-	}
+		totalEmpWage += empWage;
+		System.out.println("Emp Wage: " + empWage);
+		}
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+		}
 
 }
