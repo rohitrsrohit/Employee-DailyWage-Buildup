7d7d0e293859e17e41c33dc7dd89f16cc4149e4c
Rohit Patro <rohitpatro6@gmail.com>
2021-07-13 08:59:52 +0530

    Added:UC2 Employee Daily Wage Added With Full Time

--- src/EmployeeWageBuilder/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder/EmployeeWageBuilder.java
@@ -5,11 +5,16 @@ public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Daily Wage");
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
 		double empCheck = Math.floor(Math.random()*10)%2;
 		if(empCheck == IS_FULL_TIME)
-			System.out.println("Employee Is Present");
+			empHrs = 8;
 		else
-			System.out.println("Employee Is Not Present");
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage:" + empWage);
 	}
 
 }
