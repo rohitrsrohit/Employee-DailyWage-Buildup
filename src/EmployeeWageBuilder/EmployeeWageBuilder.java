b638794ce2f0693e69e60f22edfdbc91f252d439
Rohit Patro <rohitpatro6@gmail.com>
2021-07-19 21:32:33 +0530

    Added:UC7 Refractor Code Added

--- src/EmployeeWageBuilder/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder/EmployeeWageBuilder.java
@@ -7,12 +7,13 @@ public class EmployeeWageBuilder {
 	public static final int NUM_OF_WORKING_DAYS = 2;
 	public static final int MAX_HRS_IN_MNTH = 10;
 	
-	public static void main(String[] args) {
+	public static int computeEmpWage() {
 		System.out.println("Welcome to Employee Daily Wage");
-		
+		//Variables
 		int empHrs = 0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
+		//Computation
 		while (totalEmpHrs <= MAX_HRS_IN_MNTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
 		int empCheck =(int) Math.floor(Math.random()*10) % 3;
@@ -27,10 +28,13 @@ public class EmployeeWageBuilder {
 			empHrs = 0;
 		}
 		totalEmpHrs += empHrs;
-		System.out.println("Day# " + totalWorkingDays + "Emp Hr: " +empHrs);
+		System.out.println("Day#: " + totalWorkingDays + "Emp Hr: " +empHrs);
 		}
 		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage: " + totalEmpWage);
+		return totalEmpWage;
 		}
-
+		public static void main(String[] args) {
+			computeEmpWage();
+		}
 }
